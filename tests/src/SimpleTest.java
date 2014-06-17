2d6729d54b22f2fafb52d86955a464162102193c
Hadi Hariri <mail@hadihariri.com>
2014-06-17 16:16:59 +0200

    Prior to starting Kotlin Conversion of main server (Test converted)

--- tests/src/SimpleTest.java
+++
@@ -1,8 +0,0 @@
-import jetbrains.buildServer.BaseTestCase;
-import org.testng.annotations.Test;
-
-public class SimpleTest extends BaseTestCase {
-  @Test
-  public void Test() throws Exception {
-  }
-}
